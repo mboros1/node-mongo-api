27030c1bf383f2b32a39d51af91ff7afb715c1de
Martin Boros <mboros04@gmail.com>
2025-04-23 11:18:10 -0400

    implement working app and index entry point, endpoint works in browser

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// TODO: create express app, wire routes and db
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use('/users', require('./routes/users'));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+module.exports = app;
+
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// TODO: init express app
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
