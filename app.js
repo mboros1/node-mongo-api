f7ea18112be9ecde5d7658b899f2c962c63834fe
Martin Boros <mboros04@gmail.com>
2025-04-23 11:24:44 -0400

    app/index refactored slightly, moving db connection to index so app is more test friendly

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use('/users', require('./routes/users'));
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
 
 module.exports = app;
 
